4afe07b222dc2c46769625fc8189d6ce1b1f99fb
gracenikole <gracenikole@gmail.com>
2023-10-11 17:27:16 -0500

    first version

---
+++ app/[lang]/layout.tsx
@@ -0,0 +1,32 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Locale, i18n } from '@/i18n.config'
+import Header from './components/Header'
+
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: "MistyBlunch's Briefcase",
+}
+
+export async function generateStaticParams() {
+  return i18n.locales.map(locale => ({ lang: locale }))
+}
+
+export default function RootLayout({
+  children,
+  params
+}: {
+  children: React.ReactNode
+  params: { lang: Locale }
+}) {
+  return (
+    <html lang={params.lang}>
+      <body className={inter.className}>
+        <Header lang={params.lang} />
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}
