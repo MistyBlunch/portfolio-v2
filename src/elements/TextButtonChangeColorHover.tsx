dc23dcbeb9484e0b3eb2bde617a0bc771ce52dcf
gracenikole <gracenikole@gmail.com>
2024-01-15 02:02:16 -0500

    TextButtonChangeColorHover updated

--- src/elements/TextButtonChangeColorHover.tsx
+++ src/elements/TextButtonChangeColorHover.tsx
@@ -8,16 +8,13 @@ import { TextColorChangeDark } from './TextColorChangeDark'
 import NoSsr from './NoSsr'
 import Link from 'next/link'
 
-export const TextButtonChangeColorHover = (props: {translateDoc: string}) => {
+export const TextButtonChangeColorHover = (props: { translateDoc: string }) => {
   const { theme } = useTheme()
   const { t } = useTranslation(props.translateDoc)
 
   return (
-    <div className='mr-auto p-4 lg:w-4/5'>
-      <Link
-        href={t('cto-link')}
-        className='link flex w-fit font-semibold'
-      >
+    <div className='p-4 max-lg:mr-auto lg:m-auto lg:w-4/5 mt-2'>
+      <Link href={t('cto-link')} className='link flex w-fit font-semibold'>
         <NoSsr>
           {theme === 'dark' ? (
             <TextColorChangeDark text={t('cto-text')} />
